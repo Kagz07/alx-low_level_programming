30aa2a70305293bfb58582d8df60de6de046b315
Kagz07 <abbykagz@gmail.com>
2022-07-02 12:50:25 -0700

    write c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,1 @@
-#include <stdio.h>
 
-/
-
- * main - entry point
-
- * Return: 0 
- 
- * /
- 
- int main(void)
-
-{
-
-	puts("\"Programming is like building a multilingual puzzle");
-
-	return (0);
-
-}
